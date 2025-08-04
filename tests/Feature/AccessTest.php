2bcd863839a8d99de2d7876dbcf0b0f356ca91f7
JHenrique <henryque.pc.game@gmail.com>
2025-08-04 19:05:03 -0300

    Testar se, Estando Logado, dá Para Aceder à Página de Login (corrigido erro)

--- tests/Feature/AccessTest.php
+++ tests/Feature/AccessTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it ('test is an adimn user can see the RH users oage', function () {
+it('tests is an admin user can see the RH users page', function(){
 
     // criar o admin
     addAdminUser();
@@ -8,24 +8,29 @@ it ('test is an adimn user can see the RH users oage', function () {
     // efetuar login com o admin
     auth()->loginUsingId(1);
 
-    // verificar se acede com sucessi a pagina de RH users
+    // verifca se acede com sucesso à pagina de RH users
     expect($this->get('/rh-users')->status())->toBe(200);
+
 });
 
-it('test if is not possiblr to acces the home page homo page without logged user', function () {
-    // verifica se e possivel aceder home page
+it('tests if is not possible to acces the home page without logged user', function(){
+
+    // verifica se é possível aceder à home page
     expect($this->get('/home')->status())->toBe(302);
 
     // ou
-    expect($this->ge('/home')->status())->not()->toBe(200);
+    expect($this->get('/home')->status())->not()->toBe(200);
+
 });
 
-it('test if user logged in cain access to the login page', function () {
-   // adcicionar afmin a base de dados
+// corrigido o erro
+it('tests if user logged in can acces to the login page', function(){
+
+    // adicionar admin à base de dados
     addAdminUser();
 
-    // verifica se estalogado
+    // verifica se está logado
     auth()->loginUsingId(1);
 
-    expect($this->get('/home')->status())->toBe(200);
+    expect($this->get('/login')->status())->not()->toBe(200);
 });
