b842bc8a64cfaf09ac3d90cd407bd6924d4c3a5f
JHenrique <henryque.pc.game@gmail.com>
2025-08-05 16:06:13 -0300

    Teste com Estrutura Complexa de JSON

--- app/Services/GeneralServices.php
+++ app/Services/GeneralServices.php
@@ -21,9 +21,9 @@ class GeneralServices
 
     public static function fakeDataInJson()
     {
-        // cria 10 clientes com dadso falso
+        // cria 10 clientes com dados falsos
         $clients = [];
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $clients[] = [
                 'name' => \Faker\Factory::create()->name(),
                 'email' => \Faker\Factory::create()->email(),
@@ -33,5 +33,39 @@ class GeneralServices
         }
 
         return json_encode($clients, JSON_PRETTY_PRINT);
+    }
+
+    public static function jsonComplexData()
+    {
+        return json_encode(
+            [
+                'name' => 'José Henrique',
+                'email' => 'josehenrique@gmail.com',
+                'moradas' => [
+                    [
+                        'rua' => 'Rua 1',
+                        'cidade' => 'Lisboa',
+                        'pais' => 'Portugal'
+                    ],
+                    [
+                        'rua' => 'Rua 2',
+                        'cidade' => 'Porto',
+                        'pais' => 'Portugal'
+                    ]
+                ],
+                'telefones' => [
+                    'phones' => [
+                        '123456789',
+                        '987654321',
+                        '123456789'
+                    ],
+                    'mobiles' => [
+                        '987654321',
+                        '123456789',
+                        '987654321'
+                    ]
+                ]
+            ]
+        );
     }
 }
